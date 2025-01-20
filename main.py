a0ad047029b1339bbcb63f16fe916066f01d5e48
saxton-chris <surfde22@gmail.com>
2025-01-19 21:10:03 -0600

    Limiting FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,6 +22,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
